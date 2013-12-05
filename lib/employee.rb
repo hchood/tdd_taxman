54759beac576f69fe6a5ddc44090f4ab2c165163
Helen Hood <helen.c.hood@gmail.com>
2013-12-05 12:33:13 -0500

    Employee initialize method now works.  Note:  all number variables are converted to integers upon initialization.  May need to make sure this doesn't create rounding errors later when calculating taxes.  May need to convert to floats there.

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,10 +1,20 @@
 require 'csv'
 
 class Employee
+  attr_reader :first_name, :last_name, :annual_income, :tax_paid, :tax_rate
+
+  def initialize(first_name, last_name, annual_income, tax_paid, tax_rate)
+    @first_name = first_name
+    @last_name = last_name
+    @annual_income = annual_income.to_i
+    @tax_paid = tax_paid.to_i
+    @tax_rate = tax_rate.to_i
+  end
+
   def self.parse_data(filename)
     @@employees = []
     CSV.foreach(filename, headers: true) do |row|
-      @@employees << Employee.new()
+      @@employees << Employee.new(row["first_name"], row["last_name"], row["annual_income"], row["tax_paid"], row["tax_rate"])
     end
     @@employees
   end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -15,6 +15,18 @@ describe Employee do
   it 'creates an array of Employee objects' do
     expect(employees.first.class).to eql(Employee)
   end
+
+  it 'can instantiate an Employee object with all existing data when all data is present' do
+    expect(employees.first.first_name).to eql('Johnny')
+    expect(employees.first.last_name).to eql('Smith')
+    expect(employees.first.annual_income).to eql(120000)
+    expect(employees.first.tax_paid).to eql(28000)
+    expect(employees.first.tax_rate).to eql(38)
+  end
+
+  it 'still creates an Employee object where data is missing, but lets the value for that attribute be nil' do\
+
+  end
 end
 
 # Employee class
