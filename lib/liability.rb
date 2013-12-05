e3d549a2ab4482755e3e4d91e80cc2b5f8f83eab
Helen Hood <helen.c.hood@gmail.com>
2013-12-05 15:43:17 -0500

    altered Liability initialize method to calculate amount due vs owed

--- lib/liability.rb
+++ lib/liability.rb
@@ -1,9 +1,14 @@
 class Liability
   attr_reader :employee, :amount_due, :amount_owed
 
-  def initialize(employee, amount_due, amount_owed)
+  def initialize(employee, amount_owed)
     @employee = employee
-    @amount_due = amount_due
-    @amount_owed = amount_owed
+    if amount_owed >= 0
+      @amount_owed = amount_owed.abs
+      @amount_due = 0
+    else
+      @amount_owed = 0
+      @amount_due = amount_owed.abs
+    end
   end
 end
