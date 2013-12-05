5ee22d2772475b5909bebaa59e6584974e83dab6
Helen Hood <helen.c.hood@gmail.com>
2013-12-05 14:57:29 -0500

    liability class tests pass
    Liability class has and exposes an Employee object as @employee,
    amount_due, and amount_owed

--- spec/liability_spec.rb
+++ spec/liability_spec.rb
@@ -1,7 +1,21 @@
 require 'rspec'
 require_relative '../lib/liability'
+require_relative '../lib/employee'
 
 describe Liability do
+  an_employee = Employee.new('helen', 'hood', 0,0,0)
+  let(:liability) { Liability.new(an_employee, 1000, 0)}
+  it 'has (and exposes) an employee' do
+    expect(liability.employee).to eql(an_employee)
+  end
+
+  it 'has (and exposes) an amount due' do
+    expect(liability.amount_due). to eql(1000)
+  end
+
+  it 'has (and exposes) an amount owed' do
+    expect(liability.amount_owed). to eql(0)
+  end
 
 end
 
