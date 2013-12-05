064bbf9730d43fc4483efd0a0b186ac983ac4e99
Helen Hood <helen.c.hood@gmail.com>
2013-12-05 12:19:45 -0500

    Employee parse_data class passes tests on returning array, creating array of 5 objects from citizens.csv, and array is of Employee objects

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,1 +1,11 @@
 require 'csv'
+
+class Employee
+  def self.parse_data(filename)
+    @@employees = []
+    CSV.foreach(filename, headers: true) do |row|
+      @@employees << Employee.new()
+    end
+    @@employees
+  end
+end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,6 +1,22 @@
 require 'rspec'
 require_relative '../lib/employee'
 
+describe Employee do
+  let(:employees) { Employee.parse_data('../data/citizens.csv')}
+
+  it 'reads in data and creates an array' do
+    expect(employees.class).to eql(Array)
+  end
+
+  it 'creates an array of 5 objects' do
+    expect(employees.length).to eql(5)
+  end
+
+  it 'creates an array of Employee objects' do
+    expect(employees.first.class).to eql(Employee)
+  end
+end
+
 # Employee class
 # * can instantiate where all expected data is present
 # * handles cases where data is missing
