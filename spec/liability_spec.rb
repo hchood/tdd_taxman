6f2f654da841354c450c39a5f72f90ffcf8e5376
Helen Hood <helen.c.hood@gmail.com>
2013-12-06 09:04:50 -0500

    changed employee obj names to reflect whether they get refund
    Also moved comment

--- spec/liability_spec.rb
+++ spec/liability_spec.rb
@@ -1,6 +1,13 @@
 require 'rspec'
 require_relative '../lib/liability'
 require_relative '../lib/employee'
+require_relative '../lib/tax_calculator'
+
+# REQUIREMENTS:
+# * Liability class must expose 3 properties:
+# * employee (must be instance of Employee class)
+# * amount_due (amount due to the employee from IRS)
+# * amount_owed (amount owed to the IRS)
 
 describe Liability do
   employee_no_refund = Employee.new('helen', 'hood', 38000, 10000, 30) # => owes $1400 to IRS
@@ -10,20 +17,14 @@ describe Liability do
   let(:liability2) { TaxCalculator.liability(employee_w_refund) }
 
   it 'has (and exposes) an employee' do
-    expect(liability.employee).to eql(an_employee)
+    expect(liability1.employee).to eql(employee_no_refund)
   end
 
   it 'has (and exposes) an amount due' do
-    expect(liability.amount_due). to eql(1000)
+    expect(liability1.amount_due).to eql(0)
   end
 
   it 'has (and exposes) an amount owed' do
-    expect(liability.amount_owed). to eql(0)
+    expect(liability1.amount_owed).to eql(1400.0)
   end
-end
 
-# REQUIREMENTS:
-# * Liability class must expose 3 properties:
-# * employee (must be instance of Employee class)
-# * amount_due
-# * amount_owed
