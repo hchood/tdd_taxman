a1a4a7cd4e342e5bdd7a9e279d451c9a0fa4aa61
Helen Hood <helen.c.hood@gmail.com>
2013-12-06 09:01:57 -0500

    changed argument name to reflect amount_to_pay to gov't - was unclear before who was owed

--- lib/liability.rb
+++ lib/liability.rb
@@ -1,14 +1,14 @@
 class Liability
   attr_reader :employee, :amount_due, :amount_owed
 
-  def initialize(employee, amount_owed)
+  def initialize(employee, amount_to_pay)
     @employee = employee
-    if amount_owed >= 0
-      @amount_owed = amount_owed.abs
+    if amount_to_pay >= 0
+      @amount_owed = amount_to_pay.abs
       @amount_due = 0
     else
       @amount_owed = 0
-      @amount_due = amount_owed.abs
+      @amount_due = amount_to_pay.abs
     end
   end
 end
