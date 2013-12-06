322112dc06acb215d50f023ce7d81330e57f2be6
Helen Hood <helen.c.hood@gmail.com>
2013-12-06 13:00:40 -0500

    wrote working runner program main.rb
    Also removed comments

--- lib/main.rb
+++ lib/main.rb
@@ -1,25 +1,12 @@
 require 'csv'
 require 'rspec'
 
-# sample output:
+require_relative 'liability'
+require_relative 'employee'
+require_relative 'tax_calculator'
 
-# Johnny Smith has a tax liability of $17,600
-# Jane Doe has a tax liability of $10,200
-# Liz Lemon will receive a refund of $21,000
-# [First Name] Orsillio will receive a refund of $100
-# Eric Schmidt has a tax liability of $1,512
-
-# class TaxCalculator
-#   def self.liability(employee)
-#     # calcs taxes_owed or refund_due as a Liability.new
-#   end
-# end
-
-# def Liability
-#   attr_reader :employee (instance of Employee, :amount_due, :amount_owed
-# end
-
-# class Employee
-# end
-
-# NEED TO TEST OUTPUT TO CONSOLE?
+employees = Employee.parse_data('../data/citizens.csv')
+array_of_employee_strings = TaxCalculator.summary_info(employees)
+array_of_employee_strings.each do |string|
+  puts string
+end
