e170bf3281813c4caa03c386022a1eb5b8bc6f0a
Helen Hood <helen.c.hood@gmail.com>
2013-12-06 12:55:11 -0500

    added method to return array of string info to puts in main.rb
    summary_info method.  Also created private method money_format to chop
    off decimal places and add commas.  All tests pass

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -11,4 +11,25 @@ class TaxCalculator
     end
     liabiliity_obj
   end
+
+  def self.summary_info(employees)
+    employee_info_array = []
+    employees.each do |employee|
+      refund = TaxCalculator.liability(employee).amount_due
+      taxes_owed = TaxCalculator.liability(employee).amount_owed
+      if refund > 0
+        string = "#{employee.first_name} #{employee.last_name} will receive a refund of $#{money_format(refund)}"
+      else
+        string = "#{employee.first_name} #{employee.last_name} has a tax liability of $#{money_format(taxes_owed)}"
+      end
+      employee_info_array << string
+    end
+    employee_info_array
+  end
+
+  private
+  def self.money_format(number)
+    formatted_number = sprintf('%.0f', number.to_i)
+    formatted_number.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
+  end
 end
